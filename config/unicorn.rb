f5b113f1c3439642f185db2fa2bb469a9dcf8479
Dmitry Sukhikh <china@pozitive.biz>
2014-06-15 21:43:49 +0400

    fixed unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
-root = "/home/deploy/apps/zhaiwuzhuitao/current"
+root = "/home/deploy/apps/eqingguan/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.zhaiwuzhuitao.sock"
+listen "/tmp/unicorn.eqingguan.sock"
 worker_processes 2
 timeout 30
