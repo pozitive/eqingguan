35a4acb954fe6192397aa49ea5ac8d25cfa3ae23
Dmitry Sukhikh <china@pozitive.biz>
2014-06-15 19:46:35 +0400

    Created main page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,25 +14,5 @@
 //= require jquery_ujs
 //= require foundation
 //= require turbolinks
-//= require social-share-button
-//= require_tree .
 
 $(function(){ $(document).foundation(); });
-
-$("#featured").on("ready.fndtn.orbit", function(event) {
-  console.info("ready");
-});
-$("#featured").on("before-slide-change.fndtn.orbit", function(event) {
-    console.info("before slide change");
-    $('.slider-wrapper').css( "background-position", "-=300% 0%");
-});
-$("#featuredn").on("after-slide-change.fndtn.orbit", function(event, orbit) {
-  console.info("after slide change");
-  console.info("slide " + orbit.slide_number + " of " + orbit.total_slides);
-});
-$("#featured").on("timer-started.fndtn.orbit", function(event, orbit) {
-  console.info("timer started");
-});
-$("#featured").on("timer-stopped.fndtn.orbit", function(event, orbit) {
-  console.info("timer stopped");
-});
